5237d098a6a24787baa72244cce66bb6787435cc
fan <fan@fandeMac-mini.local>
2026-06-20 10:03:21 +0800

    chore: 更新行业资讯索引 2026/6/20 10:03:21

--- rspress.config.ts
+++ rspress.config.ts
@@ -125,6 +125,7 @@ export default defineConfig({
           text: '行业资讯',
           collapsed: false,
           items: [
+            { text: '小米动态速递(6月20日)', link: '/news/daily-report-2026-06-20-am' },
             { text: '苹果动态速递(6月20日)', link: '/news/daily-report-2026-06-20' },
             { text: '苹果动态速递(6月19日)', link: '/news/daily-report-2026-06-19-am' },
             { text: '苹果动态速递(6月19日)', link: '/news/daily-report-2026-06-19-pm' },
