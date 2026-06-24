cf6c3a91f44c912c0c64707bdd5445558710f64d
fan <fan@fandeMac-mini.local>
2026-06-24 10:03:51 +0800

    chore: 更新行业资讯索引 2026/6/24 10:03:51

--- rspress.config.ts
+++ rspress.config.ts
@@ -125,6 +125,7 @@ export default defineConfig({
           text: '行业资讯',
           collapsed: false,
           items: [
+            { text: '手机行业日报（6月24日）', link: '/news/daily-report-2026-06-24-am' },
             { text: '手机行业日报（6月24日）', link: '/news/daily-report-2026-06-24' },
             { text: '折叠屏技术前沿（6月23日）', link: '/news/daily-report-2026-06-23-am' },
             { text: '折叠屏技术前沿（6月23日）', link: '/news/daily-report-2026-06-23-pm' },
